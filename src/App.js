a3deaa4145a87d539db06947bbe7e7a86b63eb29
Laurine <laurinem.moulin@gmail.com>
2023-02-01 19:09:18 +0100

    completed but without split components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import purpleRocketImg from "./img/purple-rocket.png";
 
 const App = () => {
   const [switch1, setSwitch1] = useState(false);
@@ -7,36 +8,87 @@ const App = () => {
   const [switch3, setSwitch3] = useState(false);
 
   return (
-    <div className="App">
-      <header>Ready To Go</header>
-      <section className="switchAndGoNoGo">
-        <section className="onOff">
-          <div className="first">
-            <button
-              className={switch1 ? "purple" : ""}
-              onClick={() => {
-                setSwitch1(!switch1);
-              }}
-            >
-              ON
-            </button>
-            <button
-              onClick={() => {
-                setSwitch1(!switch1);
-              }}
-              className="purple"
-            >
-              OFF
-            </button>
-          </div>
-          <div className="second"></div>
-          <div className="third"></div>
+    <body>
+      <div className="App">
+        <header>
+          <img src={purpleRocketImg} alt="image-fusee" />
+          <p>Ready To Go</p>{" "}
+        </header>
+        <section className="switchAndGoNoGo">
+          <section className="onOff">
+            <div className="first">
+              <button
+                className={switch1 === true ? "purple" : ""}
+                onClick={() => {
+                  setSwitch1(true);
+                }}
+              >
+                ON
+              </button>
+              <button
+                onClick={() => {
+                  setSwitch1(false);
+                }}
+                className={switch1 === false ? "purple" : ""}
+              >
+                OFF
+              </button>
+            </div>
+            <div className="second">
+              <button
+                className={switch2 === true ? "purple" : ""}
+                onClick={() => {
+                  setSwitch2(true);
+                }}
+              >
+                ON
+              </button>
+              <button
+                onClick={() => {
+                  setSwitch2(false);
+                }}
+                className={switch2 === false ? "purple" : ""}
+              >
+                OFF
+              </button>
+            </div>
+            <div className="third">
+              <button
+                className={switch3 === true ? "purple" : ""}
+                onClick={() => {
+                  setSwitch3(true);
+                }}
+              >
+                ON
+              </button>
+              <button
+                onClick={() => {
+                  setSwitch3(false);
+                }}
+                className={switch3 === false ? "purple" : ""}
+              >
+                OFF
+              </button>
+            </div>
+          </section>
+          <section className="goNogo">
+            {switch1 && switch2 && switch3 ? (
+              <div className="go">Go!</div>
+            ) : (
+              <div className="noway">No way !</div>
+            )}
+          </section>
         </section>
-        <section className="goNogo"></section>
-      </section>
 
-      <footer>Made with React at Le Reacteur by Laurine</footer>
-    </div>
+        <footer>
+          <p>
+            Made with <span style={{ fontWeight: "bold" }}>React</span> at{" "}
+            <span style={{ fontWeight: "bold" }}>Le Reacteur</span> by{" "}
+            <span style={{ fontWeight: "bold" }}>Laurine</span>
+          </p>{" "}
+        </footer>
+      </div>
+    </body>
   );
 };
 
